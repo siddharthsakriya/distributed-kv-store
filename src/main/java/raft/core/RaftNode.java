c7a836a8c1c819790efa180125825088c49cf9e7
Siddharth Sakriya <siddharthsakriya@Siddharths-MacBook-Pro.local>
2025-10-23 19:21:17 +0100

    added some code to RaftNode, and added some tests to check the transitions

--- src/main/java/raft/core/RaftNode.java
+++ src/main/java/raft/core/RaftNode.java
@@ -1,9 +1,92 @@
 package raft.core;
 
-import java.util.ArrayList;
-import java.util.List;
+import raft.rpc.RaftRpcClient;
+import raft.rpc.RaftRpcServer;
+import raft.state.LeaderState;
+import raft.state.PersistentState;
+import raft.state.RaftRole;
+import raft.state.VolatileState;
 
 public class RaftNode {
+    private static int nextId = 1;
+    private final long nodeId;
+    private RaftRpcServer raftRpcServer;
+    private RaftRpcClient raftRpcClient;
+    private LeaderState leaderState;
+    private VolatileState volatileState;
+    private PersistentState persistentState;
+    private RaftRole raftRole;
+    private RaftConfig raftConfig;
+
+    public RaftNode() {
+        this.nodeId = RaftNode.nextId;
+        nextId++;
+        raftRpcServer = new RaftRpcServer();
+        raftRpcClient = new RaftRpcClient();
+        leaderState = null;
+        volatileState = new VolatileState(0,0,-1);
+        persistentState = new PersistentState();
+        raftRole = RaftRole.FOLLOWER;
+        raftConfig = null;
+    }
 
 
+
+    public static int getNextId() {
+        return nextId;
+    }
+
+    public long getNodeId() {
+        return nodeId;
+    }
+
+    public RaftRpcServer getRaftRpcServer() {
+        return raftRpcServer;
+    }
+
+    public RaftRpcClient getRaftRpcClient() {
+        return raftRpcClient;
+    }
+
+    public LeaderState getLeaderState() {
+        return leaderState;
+    }
+
+    public VolatileState getVolatileState() {
+        return volatileState;
+    }
+
+    public PersistentState getPersistentState() {
+        return persistentState;
+    }
+
+    public RaftRole getRaftRole() {
+        return raftRole;
+    }
+
+    public RaftConfig getRaftConfig() {
+        return raftConfig;
+    }
+
+    private void setRaftRole(RaftRole raftRole) {
+        this.raftRole = raftRole;
+    }
+
+    public void becomeFollower(int term) {
+        if (term > persistentState.getCurrentTerm()) {
+            persistentState.setCurrentTerm(term);
+            persistentState.resetVote();
+        }
+        setRaftRole(RaftRole.FOLLOWER);
+        leaderState = null;
+    }
+
+    public void becomeCandidate() {
+        setRaftRole(RaftRole.CANDIDATE);
+        //TODO: need to add some logic to increment term, vote for self and start voting
+    }
+
+    public void becomeLeader() {
+        setRaftRole(RaftRole.LEADER);
+    }
 }
---
+++ src/test/java/unit/raft/core/RaftNodeTest.java
@@ -0,0 +1,47 @@
+package unit.raft.core;
+
+import org.junit.jupiter.api.Test;
+import raft.core.RaftNode;
+import raft.state.PersistentState;
+import raft.state.RaftRole;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RaftNodeTest {
+    @Test
+    void testBecomeFollower() {
+        RaftNode raftNode = new RaftNode();
+
+        raftNode.getPersistentState().setCurrentTerm(10);
+        raftNode.getPersistentState().setVotedFor(8);
+
+        assertEquals(10, raftNode.getPersistentState().getCurrentTerm());
+        assertEquals(8, raftNode.getPersistentState().getVotedFor());
+
+        raftNode.becomeFollower(12);
+
+        assertEquals(12, raftNode.getPersistentState().getCurrentTerm());
+        assertEquals(-1, raftNode.getPersistentState().getVotedFor());
+        assertEquals(RaftRole.FOLLOWER, raftNode.getRaftRole());
+    }
+
+    @Test
+    void testBecomeCandidate() {
+        RaftNode raftNode = new RaftNode();
+        assertEquals(RaftRole.FOLLOWER, raftNode.getRaftRole());
+
+        raftNode.becomeCandidate();
+
+        assertEquals(RaftRole.CANDIDATE, raftNode.getRaftRole());
+    }
+
+    @Test
+    void testBecomeLeader() {
+        RaftNode raftNode = new RaftNode();
+        assertEquals(RaftRole.FOLLOWER, raftNode.getRaftRole());
+
+        raftNode.becomeLeader();
+
+        assertEquals(RaftRole.LEADER, raftNode.getRaftRole());
+    }
+}
