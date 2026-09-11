2dafab6e8ed2534d4157786e70841088aef80c3a
Siddharth Sakriya <siddharthssakriya@gmail.com>
2026-09-11 21:19:28 +0100

    added second scenario

--- raft/figure8_test.go
+++ raft/figure8_test.go
@@ -32,6 +32,29 @@ func TestFigure8CommitGuard(t *testing.T) {
 	})
 
 	t.Run("commits once current-term entry replicated", func(t *testing.T) {
+		n := NewNode(Config{ID: "n0", Peers: []string{"n1", "n2", "n3", "n4"}})
+		n.role = Leader
+		n.currentTerm = 2
 
+		n.log = []LogEntry{
+			{Index: 1, Term: 1},
+			{Index: 2, Term: 1},
+			{Index: 3, Term: 2},
+		}
+
+		n.matchIndex = map[string]int{
+			"n1": 3,
+			"n2": 3,
+			"n3": 0,
+			"n4": 0,
+		}
+
+		n.commitIndex = 0
+
+		n.advanceCommitIndex()
+
+		if n.commitIndex != 3 {
+			t.Fatalf("commitIndex is at to %d, the commit index should be %d", n.commitIndex, 3)
+		}
 	})
 }
