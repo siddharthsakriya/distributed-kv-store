284cf4eca5a37227321b0bac870736a882a1bad6
Siddharth Sakriya <siddharthssakriya@gmail.com>
2026-09-11 21:16:39 +0100

    old term entry must not be commited until new entry in current term comes

---
+++ raft/figure8_test.go
@@ -0,0 +1,37 @@
+package raft
+
+import "testing"
+
+func TestFigure8CommitGuard(t *testing.T) {
+
+	t.Run("old-term entry not committed by count", func(t *testing.T) {
+		n := NewNode(Config{ID: "n0", Peers: []string{"n1", "n2", "n3", "n4"}})
+		n.role = Leader
+		n.currentTerm = 2
+
+		n.log = []LogEntry{
+			{Index: 1, Term: 1},
+			{Index: 2, Term: 1},
+		}
+
+		n.matchIndex = map[string]int{
+			"n1": 2,
+			"n2": 2,
+			"n3": 0,
+			"n4": 0,
+		}
+
+		n.commitIndex = 0
+
+		n.advanceCommitIndex()
+
+		if n.commitIndex != 0 {
+			t.Fatalf("commitIndex advanced to %d, an old-term entry must not commit by count", n.commitIndex)
+		}
+
+	})
+
+	t.Run("commits once current-term entry replicated", func(t *testing.T) {
+
+	})
+}
